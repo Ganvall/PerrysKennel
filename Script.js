c794ffca7f6b8b066817b5dc94675dd126e1df19
JOELTVA <joeltva@hotmail.com>
2016-11-19 11:43:39 +0100

    fixade lite autoskit

--- Script.js
+++ Script.js
@@ -2,3 +2,19 @@
  * Created by JOELTVA on 2016-11-19.
  */
 
+window.onload = setDate();
+window.onload = setLastChanged();
+
+
+
+function setDate() {
+    document.getElementById("time").innerHTML=Date();
+    console.log("varför");
+}
+
+function setLastChanged() {
+    var x = document.lastModified;
+    document.getElementById("lastchanged").innerHTML= x;
+    console.log(document.lastModified)
+}
+
