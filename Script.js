5e9eda8012738e271726f6520e5ac8c011463c01
JOELTVA <joeltva@hotmail.com>
2016-11-19 10:51:20 +0100

    fixade lite autoskit

--- Script.js
+++ Script.js
@@ -2,28 +2,4 @@
  * Created by JOELTVA on 2016-11-19.
  */
 
-$("<select />").appendTo("nav");
 
-// Create default option "Go to..."
-$("<option />", {
-    "selected": "selected",
-    "value"   : "",
-    "text"    : "Go to..."
-
-}).appendTo("nav select");
-
-// Populate dropdown with menu items
-$("nav a").each(function() {
-    var el = $(this);
-    $("<option />", {
-        "value"   : el.attr("href"),
-        "text"    : el.text()
-    }).appendTo("nav select");
-});
-
-$(document).ready(function() {
-    $("#dropmenu").change(function() {
-        location = $("#dropmenu option:selected").val();
-        console.log("hejsan");
-    });
-});
