99842f4ab29c7d243c07bb912374429ff144bd80
JOELTVA <joeltva@hotmail.com>
2016-11-19 11:48:24 +0100

    fixade lite autoskit

--- Script.js
+++ Script.js
@@ -4,9 +4,6 @@
 
 window.onload = setDate();
 
-
-
-
 function setDate() {
     document.getElementById("time").innerHTML="Current time: " + Date() + ". Page Last changed: " + document.lastModified;
 }
