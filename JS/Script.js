ddf5cd914bc60bb7399b60bb6f8d48e29cb3bb68
JOELTVA <joeltva@hotmail.com>
2016-11-22 17:10:30 +0100

    fixade lite autoskit

--- JS/Script.js
+++ JS/Script.js
@@ -10,4 +10,22 @@ function setDate() {
     document.getElementById("time").innerHTML="Current time: " + Date() + ". Page Last changed: " + document.lastModified;
 }
 
+function toggleMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        console.log("hallå")
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
